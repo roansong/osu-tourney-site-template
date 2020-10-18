a68aac276094095cf24ab04e544f075e09791805
Roan Song <roansong@gmail.com>
2020-10-18 17:10:22 +0200

    Added view names

--- ozapt/urls.py
+++ ozapt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view()),
-    path("/about", views.AboutView.as_view()),
+    path("", views.IndexView.as_view(), name="home"),
+    path("/about", views.AboutView.as_view(), name="about"),
 ]
