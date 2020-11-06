d80a67fe37f17a6b76217dc108c2041b67f89666
Roan Song <roansong@gmail.com>
2020-11-07 00:05:51 +0200

    Styling tweaks. Wider maps. New mappool.

--- stats/management/commands/import_beatmap_data.py
+++ stats/management/commands/import_beatmap_data.py
@@ -59,7 +59,7 @@ class Command(BaseCommand):
                 beatmap.identifier = f"{beatmap.mod}{map_number}"
 
                 mappool, _ = models.MapPool.objects.update_or_create(
-                    name=row['pool'], defaults={'download_url': DOWNLOAD_LINKS[row['pool']]}
+                    name=row['pool'], defaults={'download_url': DOWNLOAD_LINKS.get(row['pool'], '')}
                 )
                 beatmap.mappool = mappool
                 previous_pool = mappool.name
