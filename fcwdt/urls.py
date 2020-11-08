9c3df163ac8ddbf4197bf276657f7b791b795b07
Roan Song <roansong@gmail.com>
2020-11-08 15:26:19 +0200

    Reworked front page, added links, renamed 'About' to 'Info'

--- fcwdt/urls.py
+++ fcwdt/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="home"),
     path("players", views.PlayersView.as_view(), name="players"),
     path("mappool", views.MapPoolView.as_view(), name="mappool"),
-    path("about", views.AboutView.as_view(), name="about"),
+    path("info", views.AboutView.as_view(), name="about"),
     # path("bracket", views.BracketView.as_view(), name="bracket"),
 ]
